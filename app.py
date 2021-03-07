422597fa73b9ee7f992c05b6087cdb0e7a38b94b
Sudhan <aaryansudhan@gmail.com>
2021-03-07 14:41:45 +0530

    flask

--- app.py
+++ app.py
@@ -1,61 +1,52 @@
 from sys import stdout
-import logging
 from flask import Flask, render_template, Response
 from flask_socketio import SocketIO, emit
 from PIL import Image
-import threading
-import binascii
+import threading, binascii, base64, logging, os,shutil
 from time import sleep
 from io import BytesIO
-import base64
+
+global_dict = {"folder": None,"pic_num": 0}
 
 def pil_image_to_base64(pil_image):
     buf = BytesIO()
     pil_image.save(buf, format="JPEG")
     return base64.b64encode(buf.getvalue())
 
-
 def base64_to_pil_image(base64_img):
     return Image.open(BytesIO(base64.b64decode(base64_img)))
 
+def create_storage():
+    current_directory = os.getcwd()
+    final_directory = os.path.join(current_directory, r'garbage')
+    global_dict["folder"] = final_directory
+    if not os.path.exists(final_directory):
+        os.makedirs(final_directory)
+    else:
+        shutil.rmtree(final_directory)
+        os.makedirs(final_directory)
 class Camera(object):
     def __init__(self, makeup_artist):
         self.to_process = []
         self.to_output = []
         self.makeup_artist = makeup_artist
-
         thread = threading.Thread(target=self.keep_processing, args=())
         thread.daemon = True
         thread.start()
-
     def process_one(self):
         if not self.to_process:
             return
-
-        # input is an ascii string. 
         input_str = self.to_process.pop(0)
-
-        # convert it to a pil image
         input_img = base64_to_pil_image(input_str)
-
-        ################## where the hard work is done ############
-        # output_img is an PIL image
         output_img = self.makeup_artist.apply_makeup(input_img)
-
-        # output_str is a base64 string in ascii
         output_str = pil_image_to_base64(output_img)
-
-        # convert eh base64 string in ascii to base64 string in _bytes_
         self.to_output.append(binascii.a2b_base64(output_str))
-
     def keep_processing(self):
         while True:
             self.process_one()
             sleep(0.01)
-
     def enqueue_input(self, input):
         self.to_process.append(input)
-
     def get_frame(self):
         while not self.to_output:
             sleep(0.05)
@@ -64,9 +55,8 @@ class Camera(object):
 class Makeup_artist(object):
     def __init__(self):
         pass
-
     def apply_makeup(self, img):
-        return img.transpose(Image.FLIP_LEFT_RIGHT)
+        return img.transpose(Image.FLIP_TOP_BOTTOM)
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(stdout))
@@ -75,44 +65,25 @@ app.config['DEBUG'] = True
 socketio = SocketIO(app)
 camera = Camera(Makeup_artist())
 
-
-
 @socketio.on('input image', namespace='/test')
 def test_message(input):
     input = input.split(",")[1]
     camera.enqueue_input(input)
-    image_data = input # Do your magical Image processing here!!
-    #image_data = image_data.decode("utf-8")
+    image_data = input
+    with open(os.path.join(global_dict["folder"] ,"Record{0}.png".format(global_dict["pic_num"])), "wb") as fh:
+        global_dict["pic_num"] += 1
+        fh.write(base64.urlsafe_b64decode(image_data))
     image_data = "data:image/jpeg;base64," + image_data
-    # print("OUTPUT " + image_data)
     emit('out-image-event', {'image_data': image_data}, namespace='/test')
-    #camera.enqueue_input(base64_to_pil_image(input))
-
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
     app.logger.info("client connected")
 
-
 @app.route('/')
 def index():
-    """Video streaming home page."""
     return render_template('index.html')
 
-
-def gen():
-    app.logger.info("starting to generate frames!")
-    while True:
-        frame = camera.get_frame() #pil_image_to_base64(camera.get_frame())
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
 if __name__ == '__main__':
+    create_storage()
     socketio.run(app,use_reloader=False)
